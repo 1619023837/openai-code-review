8c01b36dc0b07d8dfe381f13f96ea1eb9266ca91
郝文涛 <haowentao@wondersoft.cn>
2025-03-14 13:42:08 +0800

    feat: init project

---
+++ openai-code-review-sdk/src/main/java/hao/wen/tao/sdk/OpenAiCodeReview.java
@@ -0,0 +1,11 @@
+package hao.wen.tao.sdk;
+
+
+
+public class OpenAiCodeReview
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ openai-code-review-test/src/main/java/hao/wen/tao/Application.java
@@ -0,0 +1,9 @@
+package hao.wen.tao;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
