35aba82460f3e619782c5f659046cd4aa0f5ff76
郝文涛 <haowentao@wondersoft.cn>
2025-04-16 10:19:17 +0800

    fix 问题修复

--- openai-code-review-sdk/src/main/java/hao/wen/tao/sdk/types/utils/WXAccessTokenUtils.java
+++ openai-code-review-sdk/src/main/java/hao/wen/tao/sdk/types/utils/WXAccessTokenUtils.java
@@ -14,48 +14,48 @@ public class WXAccessTokenUtils
     private static final String APP_ID = "wx68eb13f81b27a798";
     private static final String SECRET = "f86566ce071357365c4a2649f9701287";
     private static final String GRANT_TYPE = "client_credential";
-    private static final String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=%s&appid=%s&secret=%s";
+    private static final String URL_TEMPLATE  = "https://api.weixin.qq.com/cgi-bin/token?grant_type=%s&appid=%s&secret=%s";
 
-    public static String getAccessToken()
-    {
+    public static String getAccessToken() {
+        return getAccessToken(APP_ID, SECRET);
+    }
 
-        try
-        {
-            String urlStr = String.format(url, GRANT_TYPE, APP_ID, SECRET);
-            URL url = new URL(urlStr);
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+    public static String getAccessToken(String APPID, String SECRET) {
+        try {
+            String urlString = String.format(URL_TEMPLATE, GRANT_TYPE, APPID, SECRET);
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
-            System.out.println("responseCode:" + responseCode);
-            if (responseCode == HttpURLConnection.HTTP_OK){
-                try(BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream())))
-                {
-                    String line = "";
-                    StringBuilder response = new StringBuilder();
-                    while ((line = bufferedReader.readLine()) != null){
-                        response.append(line);
-                    }
-                    // Print the response
-                    System.out.println("Response: " + response.toString());
-                    Token token = JSON.parseObject(response.toString(), Token.class);
-                    return token.getAccess_token();
-                }
+            System.out.println("Response Code: " + responseCode);
 
-            }else
-            {
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Print the response
+                System.out.println("Response: " + response.toString());
+
+                Token token = JSON.parseObject(response.toString(), Token.class);
+
+                return token.getAccess_token();
+            } else {
                 System.out.println("GET request failed");
                 return null;
             }
-        }
-        catch (Exception  e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
-
         }
     }
+
     public static class Token {
         private String access_token;
         private Integer expires_in;
@@ -76,6 +76,8 @@ public class WXAccessTokenUtils
             this.expires_in = expires_in;
         }
     }
+
+
 
 
 
