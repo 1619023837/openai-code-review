974b66418862b502c9bc092ab37a889f3c743170
郝文涛 <haowentao@wondersoft.cn>
2025-04-30 15:58:02 +0800

    fix: 取消日志打印token

--- openai-code-review-sdk/src/main/java/hao/wen/tao/sdk/infrastructure/git/GitRestAPIOperation.java
+++ openai-code-review-sdk/src/main/java/hao/wen/tao/sdk/infrastructure/git/GitRestAPIOperation.java
@@ -38,8 +38,6 @@ public class GitRestAPIOperation implements BaseGitOperation
         header.put("Accept", "application/vnd.github+json");
         header.put("Authorization", "Bearer " + token);
         header.put("X-GitHub-Api-Version", "2022-11-28");
-        logger.error("url: " + url);
-        logger.error("token: " + token);
         String execute = DefaultHttpUtil.execute(url, header);
         SingleCommitResponse singleCommitResponse = JSON.parseObject(execute,
             SingleCommitResponse.class);
